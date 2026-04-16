ac7ca6270559ad3c2eba46545fc8043583d768d0
yangguo <yangguo@noxgroup.com>
2026-04-16 16:53:20 +0800

    refactor: remove obsolete bridge constants
    Remove STATE_DIR_NAME, SOCKET_FILE_NAME, PID_FILE_NAME,
    LOG_FILE_NAME, DEFAULT_TCP_PORT that are no longer used
    after bridge removal.

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -14,18 +14,8 @@ const SNAPSHOT_STATUS = Object.freeze({
 });
 
 const VALID_PLAN_LEVELS = new Set(['lite', 'pro', 'max', 'unknown']);
-const STATE_DIR_NAME = '.glm-safe-statusline';
-const SOCKET_FILE_NAME = 'bridge.sock';
-const PID_FILE_NAME = 'bridge.pid';
-const LOG_FILE_NAME = 'bridge.log';
-const DEFAULT_TCP_PORT = 45219;
 
 module.exports = {
-  DEFAULT_TCP_PORT,
-  LOG_FILE_NAME,
-  PID_FILE_NAME,
   SNAPSHOT_STATUS,
-  SOCKET_FILE_NAME,
-  STATE_DIR_NAME,
   VALID_PLAN_LEVELS,
 };
